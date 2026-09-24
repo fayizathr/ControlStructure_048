bccd9782a799f6c7054a348aa4f46797591cb541
fairuzathrz <fayizpdp2008@gmai.com>
2026-09-24 11:08:49 +0700

    First commit

---
+++ No1.py
@@ -0,0 +1,12 @@
+nilai = int(input("Masukkan nilai: "))
+
+if nilai >= 90:
+    print("Excellent performance")
+elif nilai >= 80:
+    print("Very Good performance")
+elif nilai >= 70:
+    print("Good performance")
+elif nilai >= 60:
+    print("Average performance")
+else:
+    print("Below average performance")
